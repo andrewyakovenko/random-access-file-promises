9ff923d9290884d4bda31220d837f3e8ae1af949
andrewyakovenko <andrew.yakovenko@gmail.com>
2024-01-10 22:42:14 +0200

    returning from withRanfomAccessFile

--- src/util.ts
+++ src/util.ts
@@ -1,9 +1,9 @@
 import {RandomAccessFile, RandomAccessFileOptions} from './random-access-file';
 
-export async function withRandomAccessFile(path: string, fn: (raf: RandomAccessFile) => Promise<void>, opts: RandomAccessFileOptions = {}): Promise<void> {
+export async function withRandomAccessFile<T>(path: string, fn: (raf: RandomAccessFile) => Promise<T>, opts: RandomAccessFileOptions = {}): Promise<T> {
     const raf = new RandomAccessFile(path, opts);
     try {
-        await fn(raf);
+        return await fn(raf);
     } finally {
         await raf.close();
     }
